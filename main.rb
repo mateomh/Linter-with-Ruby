7a478dc124b38101059f8f3d55fd5b1934c5846f
mateomh <mmojicah.84@gmail.com>
2020-08-25 10:05:25 -0500

    Fix linter errors for the initial checks

--- main.rb
+++ main.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 require 'find' # Gets the find module
 
-raise StandardError.new("Too many arguments") if ARGV.length > 1
+raise StandardError, 'Too many arguments' if ARGV.length > 1
 
 html_files = []
 css_files = []
 
 if !ARGV.empty?
-  raise StandardError.new("File does NOT exist") if !File.exists?(ARGV[0])
-  html_files.push(ARGV[0]) 
+  raise StandardError, 'File does NOT exist' unless File.exist?(ARGV[0])
+
+  html_files.push(ARGV[0])
 else
   Find.find('./') do |path|
-    html_files.push(File.open(path, 'r')) if File.extname(path) == ".html"
-    css_files.push(File.open(path, 'r')) if File.extname(path) == ".css"
+    html_files.push(File.open(path, 'r')) if File.extname(path) == '.html'
+    css_files.push(File.open(path, 'r')) if File.extname(path) == '.css'
   end
 end
 
-raise StandardError.new("No files to check in the folder")if html_files.empty?
+raise StandardError, 'No files to check in the folder' if html_files.empty?

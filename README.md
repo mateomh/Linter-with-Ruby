f3e6668d90572264461aa45b80de4447ccc7bc17
mateomh <mmojicah.84@gmail.com>
2020-08-26 23:08:29 -0500

    Add structure to the readme file and added MIT license

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Linter-with-Ruby
-Microverse Capstone project for the Ruby module
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# CSS Linter
+
+> A basic linter for CSS files developed using ruby.
+
+![animation](./imgs/search.gif)
+
+This project was developed as a capstone project for the Microverse Ruby Module. The objective is to create a linter using the ruby language and test it using Rspec.
+
+## Built With
+
+- Ruby,
+- Rspec,
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+### Setup
+
+### Usage
+
+### Run tests
+
+
+## Author
+
+👤 **Mateo Mojica**
+- Github: [@mateomh](https://github.com/mateomh)
+- Twitter: [@mateo_m_h](https://twitter.com/mateo_m_h)
+- Linkedin: [Mateo Mojica](https://linkedin.com/mateo_mojica_hernandez)
+
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](https://opensource.org/licenses/MIT) licensed.
--- bin/mm_css_lint.rb
+++ bin/mm_css_lint.rb
@@ -1,34 +1,42 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/BlockLength
 require 'find' # Gets the find module to get all the files in the folder
 require './lib/lint.rb'
 
+# Checks for the number of arguments passed
 raise StandardError, 'Too many arguments' if ARGV.length > 1
 
 html_files = []
 css_files = []
 
 if !ARGV.empty?
+  # Checks if the file name passed exists
   raise StandardError, 'File does NOT exist' unless File.exist?(ARGV[0])
 
   css_files.push(ARGV[0])
 else
+  # If no argument passed gets all the files in the project folder
   Find.find('./') do |path|
     html_files.push(File.open(path, 'r')) if File.extname(path) == '.html'
     css_files.push(File.open(path, 'r')) if File.extname(path) == '.css'
   end
 end
 
+# Checks if there are files to check
 raise StandardError, 'No files to check in the folder' if css_files.empty?
 
+# Cycles through the files found in the project folder
 css_files.each do |filename|
   current_file = File.open(filename, 'r')
 
   linter = Lint.new(File.basename(current_file))
 
+  # Reads the file line by line
   current_file.each_with_index do |text, line|
     linter.curr_text = text
     linter.curr_line = line
     line_type = linter.type_of_line
+    # Calls the linters acording to the type of line
     case line_type
     when 'comment'
       linter.empty_reset
@@ -60,4 +68,4 @@ css_files.each do |filename|
   end
   linter.report
 end
-exit(0)
+# rubocop:enable Metrics/BlockLength
